c7d5d96815292a9771f5bd8ec05d0e9697762ee6
PabloJz98xD <juan-pablo-z-s58@hotmail.com>
2022-02-17 17:42:26 -0600

    first commit

---
+++ src/main/java/com/practica/mensajes_app/Conexion.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.practica.mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pablojz
+ */
+class Conexion {
+    public Connection get_connection(){
+    Connection connection = null;
+    try{
+    connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app","root","");
+            if (connection != null) {
+                System.out.println("conexion exitosa");
+            }
+                    }catch(SQLException e){
+                    System.out.println(e);
+                    }
+            return connection;
+}   
+}
